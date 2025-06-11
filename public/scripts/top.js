468a8f9a277b05174b5aec08099b45b5380b6088
JPiedra <javierpiedraiglesias@gmail.com>
2025-06-11 10:04:08 +0200

    adding js

--- public/scripts/top.js
+++ public/scripts/top.js
@@ -16,16 +16,3 @@ scrollTOP?.addEventListener("click", (e) => {
         behavior: "smooth",
     })
 })
-
-// Esta parte es para cambiar el estilo de la barra de navegación al hacer scroll
-window.addEventListener("scroll", () => {
-    const navBar = document.getElementById("navbar")
-    if (!navBar) return
-    if (window.scrollY > 50) {
-        navBar.classList.add("bg-[#0a2069]", "shadow-md", "text-white")
-        navBar.classList.remove("bg-transparent", "text-white")
-    } else {
-        navBar.classList.remove("bg-[#0a2069]", "shadow-md", "text-white")
-        navBar.classList.add("bg-transparent", "text-white")
-    }
-})
